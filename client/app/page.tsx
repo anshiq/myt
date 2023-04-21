553169db472016efca3dd7f497b79bf0e1056e59
anshiq <anshikthind@gmail.com>
2023-04-21 22:39:44 +0530

    files structure setup

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+import Card from './component/card'
+export default function Body() {
+// const k =  Card("ansik"); 
+   return (
+    <>
+ <div  className="grid grid-cols-1 gridXs:grid-cols-2 gridMd:grid-cols-3 gridLd:grid-cols-4">allposts</div>
+      </>
+  );
+}
